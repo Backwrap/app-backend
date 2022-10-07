e3f7a6ea8df29524560c343928ccace21d0d2692
Ojo Peter <ojopeter36@gmail.com>
2022-10-07 10:11:01 +0100

    feature update

---
+++ models/user.model.js
@@ -0,0 +1,41 @@
+/*jshint esversion: 6 */
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    firstName :{
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName :{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email :{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        validate:{
+            validator: (value)=>{
+                const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+                return value.match(re);
+            },
+            message: 'Please enter a valid email address',
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: (value)=>{
+            return value.length >= 8;
+        },
+        message: 'Password must be at least 6 characters long',
+    },
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 /*jshint esversion: 6 */
 
 const app = require("express")();
+const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
+const userRouter = require("./routes/user.route");
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/user', userRouter);
 
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`);
