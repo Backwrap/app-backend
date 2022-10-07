833966780e4a07c56bb200c2249f34791edeb1bb
Ojo Peter <ojopeter36@gmail.com>
2022-10-07 15:36:56 +0100

    feature update

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const router = express.Router();
-const {registerUser} = require('../controllers/user.controller');
+const {registerUser, loginUser} = require('../controllers/user.controller');
 
 
 
-router.post('/api/signup', registerUser);
+router.post('/register', registerUser);
+router.post('/signin', loginUser);
 
 module.exports = router;
