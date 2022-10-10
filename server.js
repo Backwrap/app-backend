4e25a8ecf1fb23a56213382c28ba2407c1c3d948
Ojo Peter <ojopeter36@gmail.com>
2022-10-10 13:58:02 +0100

    hotfix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
 
-const app = require("express")();
+const express = require("express");
+const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/user', userRouter);
+
 
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`);
